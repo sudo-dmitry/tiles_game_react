5c6680f63c5256dc8cce34071ded6ba59c5b3ffd
sudo-dmitry <dmitry.v.pletnev@gmail.com>
2020-08-03 13:17:33 +0300

    Updates

---
+++ precache-manifest.7f5c52de53083228e41a65d771619a74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b8475058bdf00eb7ed735f6f8ef9d5",
+    "url": "/tiles_game_react/index.html"
+  },
+  {
+    "revision": "5170f756e3c213264727",
+    "url": "/tiles_game_react/static/css/main.a364528c.chunk.css"
+  },
+  {
+    "revision": "aa0c066700bd4f1007db",
+    "url": "/tiles_game_react/static/js/2.5948b4c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tiles_game_react/static/js/2.5948b4c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5170f756e3c213264727",
+    "url": "/tiles_game_react/static/js/main.dbabcbd6.chunk.js"
+  },
+  {
+    "revision": "aa895a5e64dcd376d9c9",
+    "url": "/tiles_game_react/static/js/runtime-main.73026620.js"
+  }
+]);
